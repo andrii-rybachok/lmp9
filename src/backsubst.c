25059236437d4a21e06b8e9cde460a1c056d34c0
andrii-rybachok <80616761+andrii-rybachok@users.noreply.github.com>
2023-12-19 18:28:53 +0100

    aktualizuje swoje repozytorium oraz dodaje zmiany do repozytorium zdalnego

--- src/backsubst.c
+++ src/backsubst.c
@@ -5,17 +5,24 @@
  * Zwraca 2 - błąd nieprawidłowych rozmiarów macierzy
  */
 int  backsubst(Matrix *x, Matrix *mat, Matrix *b) {
-				/**
-				 * Tutaj należy umieścić właściwą implemntację.
-				 */
 
 				/* To ponizej jest przepisaniem b do x. Nalezy to poprawic! */
-
-				int i;
-				for (i =0; i < x->r; i++) {
-								x->data[i][0] = b->data[i][0];
+				if(mat->c!=x->r || mat->c!=b->r){
+					return 2;
 				}
-
+				x->data[b->r-1][0] =b->data[b->r-1][0]/ mat->data[mat->r-1][mat->c-1];
+				for (int j = mat->r-2; j >= 0; j--)
+				{
+					double rowSum = 0;
+					for (int i = j+1; i < mat->r; j++)
+					{
+						rowSum += mat->data[j][i]*x->data[i][0];
+					}
+					if(mat->data[j][j]==0){
+						return 1;
+					}
+					x->data[j][0] = (b->data[j][0]-rowSum)/mat->data[j][j]; 
+				}
 				return 0;
 }
 
