ce3ad03e4a274391a32ecd566759567de0332820
andrii-rybachok <80616761+andrii-rybachok@users.noreply.github.com>
2023-12-19 18:52:00 +0100

    poprawiłem błąd w funkcji podstawienia wstecznego

--- src/backsubst.c
+++ src/backsubst.c
@@ -1,4 +1,6 @@
 #include "backsubst.h"
+#include <stdio.h>
+#include <stdlib.h>
 /**
  * Zwraca 0 - wsteczne podstawienie zakonczone sukcesem
  * Zwraca 1 - błąd dzielenia przez 0 (element na diagonali = 0)
@@ -10,14 +12,16 @@ int  backsubst(Matrix *x, Matrix *mat, Matrix *b) {
 				if(mat->c!=x->r || mat->c!=b->r){
 					return 2;
 				}
-				x->data[b->r-1][0] =b->data[b->r-1][0]/ mat->data[mat->r-1][mat->c-1];
+				x->data[x->r-1][0] =b->data[b->r-1][0]/ mat->data[mat->r-1][mat->c-1];
 				for (int j = mat->r-2; j >= 0; j--)
 				{
 					double rowSum = 0;
-					for (int i = j+1; i < mat->r; j++)
+
+					for (int i = j+1; i < mat->r; i++)
 					{
 						rowSum += mat->data[j][i]*x->data[i][0];
 					}
+
 					if(mat->data[j][j]==0){
 						return 1;
 					}
