efb93fdf935b6c21f64c9b541e10bf4575451c5f
SkyForce06 <01192876@pw.edu.pl>
2023-12-21 00:05:19 +0100

    Rozszerzenie metody eliminacji Gaussa o wybór elementu głównego

--- src/gauss.c
+++ src/gauss.c
@@ -1,28 +1,45 @@
 #include "gauss.h"
-
+#include "math.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 int eliminate(Matrix *mat, Matrix *b) {
     if (mat->r != mat->c || mat->r != b->r) {
         return 2; // Błąd nieprawidłowych rozmiarów macierzy
     }
+    double mainEl = mat->data[0][0];
+    int indexForChange=0;
+    for (int i = 0; i < mat->r; i++)
+    {
+        double absCurr=fabs(mat->data[i][0]);
 
-    for (int k = 0; k < mat->r - 1; k++) {
-        int maxRow = k;
-        for (int i = k + 1; i < mat->r; i++) {
-            if (fabs(mat->data[i][k]) > fabs(mat->data[maxRow][k])) {
-                maxRow = i;
+        if(mainEl<absCurr){
+            mainEl = absCurr;
+            indexForChange=i;
+
+        }
+        
+    }
+    if(indexForChange!=0){
+        
+        double tmp = b->data[0][0];
+        b->data[0][0] =  b->data[indexForChange][0];
+        b->data[indexForChange][0] = tmp;
+
+        for (int j = 0; j < mat->r; j++)
+        {
+            for (int i = 0; i < mat->c; i++)
+            {
+                double tmp = mat->data[j][i];
+                mat->data[j][i] =  mat->data[indexForChange][i];
+                mat->data[indexForChange][i] = tmp;
             }
+            
         }
-
-        for (int j = k; j < mat->r; j++) {
-            double temp = mat->data[k][j];
-            mat->data[k][j] = mat->data[maxRow][j];
-            mat->data[maxRow][j] = temp;
-        }
-
-        double temp = b->data[k][0];
-        b->data[k][0] = b->data[maxRow][0];
-        b->data[maxRow][0] = temp;
-
+        
+    }
+    
+    for (int k = 0; k < mat->r - 1; k++) {
         for (int i = k + 1; i < mat->r; i++) {
             if (mat->data[k][k] == 0) {
                 return 1; // Błąd dzielenia przez 0 (element na diagonali = 0)
